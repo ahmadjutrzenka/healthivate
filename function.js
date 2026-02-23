e96e6967aaa01e18fe8acea77fd2ded4faae0025
unknown <ahluthfiha2412@gmail.com>
2026-02-23 15:09:41 +0700

    Seperate & Polish Style

--- function.js
+++ function.js
@@ -1,16 +1,18 @@
 let btnBMI = document.getElementById("btn");
-let BMIOutput = document.getElementById("bmi-result");
+// let BMIOutput = document.getElementById("bmi-result");
 let result = document.getElementById("result");
 let workout_list = document.getElementById("workout-list");
 let container2 = document.getElementById("container-2");
 
-function calculateBMI() {
+btnBMI.addEventListener("click", function calculateBMI() {
     console.log("clicked");
-    const heightValue = document.getElementById("height").value / 100;
-    const weightValue = document.getElementById("weight").value;
+    let heightValue = document.getElementById("height").value / 100;
+    let weightValue = document.getElementById("weight").value;
 
-    const BMIValue = weightValue / (heightValue * heightValue);
+    let BMIValue = weightValue / (heightValue * heightValue);
     // BMIOutput.value = BMIValue.toFixed(2);
+
+    if(!heightValue || !weightValue) return;
 
     if(BMIValue < 18.5) {
         result.innerHTML = `
@@ -18,25 +20,84 @@ function calculateBMI() {
         <p style="font-size: 25px;"> Underweight </p>
         `
         workout_list.innerHTML = `
+            <li>Push Up - 3 set x 10-15 rep - membangun otot dada dan trisep</li>
+            <li>Squat - 3 set x 12 rep - membentuk otot kaki</li>
+            <li>Plank - 3 set x 30 detik - memperkuat otot inti</li>
+            <li>Lunges - 3 set x 10 per kaki - melatih keseimbangan</li>
+            <li>Yoga Ringan - 20 menit - meningkatkan fleksibilitas</li>
+        `
+        container2.style.display = "";
+        
+    }
+    else if(BMIValue < 25) {
+        result.innerHTML = `
+        <strong style="font-size: 30px; display: block; text-align: center"> ${BMIValue.toFixed(2)} </strong>
+        <p style="font-size: 25px;"> Normal </p>
+        `
+        workout_list.innerHTML = `
             <li>Perbanyak konsumsi makan-makanan
             <li>Kurangin latihan kardio
             <li>Perbanyak asupan protein
         `
         container2.style.display = "";
-        // outputBMI.innerHTML = "Underweight"
-        // outputWO1.innerHTML = "- Perbanyak makan buah-buahan"
     }
-    else if(BMIValue >= 18.5 && BMIValue <= 24.9) {
-        // outputBMI.innerHTML = "Normal"
-    }
-    else if(BMIValue >= 25 && BMIValue <= 29.9) {
-        // outputBMI.innerHTML = "Overweight"
+    else if(BMIValue < 30) {
+        result.innerHTML = `
+        <strong style="font-size: 30px; display: block; text-align: center"> ${BMIValue.toFixed(2)} </strong>
+        <p style="font-size: 25px;"> Overweight </p>
+        `
+        workout_list.innerHTML = `
+            <li>Perbanyak konsumsi makan-makanan
+            <li>Kurangin latihan kardio
+            <li>Perbanyak asupan protein
+        `
+        container2.style.display = "";
     }
     else {
-        // outputBMI.innerHTML = "Obesity"
+        result.innerHTML = `
+        <strong style="font-size: 30px; display: block; text-align: center"> ${BMIValue.toFixed(2)} </strong>
+        <p style="font-size: 25px;"> Obesitas </p>
+        `
+        workout_list.innerHTML = `
+            <li>Perbanyak konsumsi makan-makanan
+            <li>Kurangin latihan kardio
+            <li>Perbanyak asupan protein
+        `
     } 
 
-    console.log(BMIValue);
-}
+})
 
-btnBMI.addEventListener('click', calculateBMI);
+// function calculateBMI() {
+//     console.log("clicked");
+//     let heightValue = document.getElementById("height").value / 100;
+//     let weightValue = document.getElementById("weight").value;
+
+//     const BMIValue = weightValue / (heightValue * heightValue);
+//     // BMIOutput.value = BMIValue.toFixed(2);
+
+//     if(BMIValue < 18.5) {
+//         result.innerHTML = `
+//         <strong style="font-size: 30px; display: block; text-align: center"> ${BMIValue.toFixed(2)} </strong>
+//         <p style="font-size: 25px;"> Underweight </p>
+//         `
+//         workout_list.innerHTML = `
+//             <li>Perbanyak konsumsi makan-makanan
+//             <li>Kurangin latihan kardio
+//             <li>Perbanyak asupan protein
+//         `
+//         container2.style.display = "";
+//         // outputBMI.innerHTML = "Underweight"
+//         // outputWO1.innerHTML = "- Perbanyak makan buah-buahan"
+//     }
+//     else if(BMIValue >= 18.5 && BMIValue <= 24.9) {
+//         // outputBMI.innerHTML = "Normal"
+//     }
+//     else if(BMIValue >= 25 && BMIValue <= 29.9) {
+//         // outputBMI.innerHTML = "Overweight"
+//     }
+//     else {
+//         // outputBMI.innerHTML = "Obesity"
+//     } 
+// }
+
+// btnBMI.addEventListener('click', calculateBMI);
