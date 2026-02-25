5ae8cd7d09aeb0f53a4fca2d56e8ee8a92f562bf
unknown <ahluthfiha2412@gmail.com>
2026-02-25 15:18:25 +0700

    luthfi-bmi-calculator

--- function.js
+++ function.js
@@ -1,22 +1,23 @@
-/** Global variable */
+/** Global Variable */
 const keyDataWorkout = "data-workout";
 let plannerForm = document.getElementById("planner-form");
 let workoutData = {
   name: null,
   category: null,
   duration: null,
-  date: "",
+  date: null,
   notes: null,
 };
 
-/** get items */
+/** Get Items */
 const dataWorkout = JSON.parse(localStorage.getItem(keyDataWorkout));
+let editIndex = null;
 
-/** functions */
+/** Functions */
 function readDataTable() {
   let list = document.getElementById("workout-body");
 
-  if (dataWorkout === null) {
+  if (dataWorkout === null || dataWorkout.length === 0) {
     let row = document.createElement("tr");
     row.innerHTML = `<td colspan="7">Data tidak tersedia</td> `;
     list.appendChild(row);
@@ -24,18 +25,16 @@ function readDataTable() {
     dataWorkout.forEach((data, idx) => {
       let row = document.createElement("tr");
       row.innerHTML = `<td>${idx + 1}</td>
-                    <td>${data.date ?? "Tanggal kosong"}</td> 
-                    <td>${data.name}</td> 
-                    <td>${data.category ?? "kategiry kosong"}</td>
-                    <td>${data.duration ?? "kategiry kosong"}</td>
-                    <td>${data.notes ?? "catatan kosong"}</td>
+                    <td>${data.date ?? "Tanggal Kosong"}</td> 
+                    <td>${data.name ?? "Nama Kosong"}</td> 
+                    <td>${data.category ?? "Kategori Kosong"}</td>
+                    <td>${data.duration ?? "Durasi Kosong"}</td>
+                    <td>${data.notes ?? "Catatan Kosong"}</td>
                     <td>
-                        <button class="btn btn-warning btn-sm edit">Edit</button>
-                        <button id="${idx}" class="btn btn-danger btn-sm delete">Delete</button>
+                        <button class="btn btn-warning btn-sm edit" onclick="editData(${idx})">Edit</button>
+                        <button id="del-btn-${idx}" class="btn btn-danger btn-sm delete" onclick="deleteData(${idx})">Delete</button>
                     </td>
                     `;
-
-      //   row.setAttribute("id", idx + 1);
       list.appendChild(row);
     });
   }
@@ -68,8 +67,48 @@ function createData(data) {
   }
 }
 
-function deleteData() {}
-/** end functions */
+function updateData(data) {
+  let newData = dataWorkout;
+  newData[editIndex] = data;
+  storeLocalStorage(keyDataWorkout, newData);
+  editIndex = null;
+  window.location.reload();
+}
+
+function deleteData(id) {
+  console.log(id);
+  let data = dataWorkout;
+  let newData = [];
+  for (let i = 0; i < data.length; i++) {
+    if (i != id) {
+      newData.push(data[i]);
+    }
+  }
+
+  console.log(newData);
+  storeLocalStorage(keyDataWorkout, newData);
+  window.location.reload();
+}
+
+function editData(id) {
+  editIndex = id;
+  let data = dataWorkout[id];
+
+  document.getElementById("workout-name").value = data.name;
+  document.getElementById("workout-category").value = data.category;
+  document.getElementById("workout-duration").value = data.duration;
+  document.getElementById("workout-notes").value = data.notes;
+  document.getElementById("workout-date").value = data.date;
+  document.getElementById("btn-2").textContent = "Edit Workout";
+
+  workoutData.name = data.name;
+  workoutData.category = data.category;
+  workoutData.duration = data.duration;
+  workoutData.notes = data.notes;
+  workoutData.date = data.date;
+}
+
+/** End Functions */
 
 /** Get Element HTML */
 document.getElementById("workout-name").addEventListener("change", (e) => {
@@ -91,21 +130,18 @@ document.getElementById("workout-notes").addEventListener("change", (e) => {
 document.getElementById("workout-date").addEventListener("change", (e) => {
   workoutData.date = e.target.value;
 });
-
-let tbody = document.getElementById("workout-body");
-let td = tbody.getElementsByTagName("td");
-console.log(td[td.length - 1]);
-
 /** END Get Element HTML */
 
 /** Event Listener */
 plannerForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  createData(workoutData); // fungsi untuk create data
+  if (editIndex === null) {
+    createData(workoutData); // fungsi untuk create data
+  } else updateData(workoutData);
 });
 
-/** End vent Listener */
+/** End Event Listener */
 
-// pemanggilan fungsi
+// Pemanggilan Fungsi
 readDataTable();
